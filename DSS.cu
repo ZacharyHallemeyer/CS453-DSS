83d13a74c71e4986e2c0b818ae8489955539058f
Num0Programmer <asajh4@gmail.com>
2024-04-14 19:35:49 -0700

    Reformatted DSS.cu because my foss text editor refuses to format corpo code.

--- DSS.cu
+++ DSS.cu
@@ -12,11 +12,11 @@
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
 {
-   if (code != cudaSuccess)
-   {
-      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-      if (abort) exit(code);
-   }
+    if (code != cudaSuccess)
+    {
+        fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+        if (abort) exit(code);
+    }
 }
 
 //Mode 0 is CPU implementation
@@ -35,110 +35,114 @@ void checkParams(unsigned int N, unsigned int DIM);
 
 int main(int argc, char *argv[])
 {
-  printf("\nMODE: %d", MODE);
-  warmUpGPU();
+    printf("\nMODE: %d", MODE);
+    warmUpGPU();
 
-  char inputFname[500];
-  unsigned int N=0;
-  unsigned int DIM=0;
-  float epsilon=0;
-
-
-  if (argc != 5) {
-    fprintf(stderr,"Please provide the following on the command line: N (number of lines in the file), dimensionality (number of coordinates per point), epsilon, dataset filename.\n");
-    exit(0);
-  }
-
-  sscanf(argv[1],"%d",&N);
-  sscanf(argv[2],"%d",&DIM);
-  sscanf(argv[3],"%f",&epsilon);
-  strcpy(inputFname,argv[4]);
-
-  checkParams(N, DIM);
-
-  printf("\nAllocating the following amount of memory for the dataset: %f GiB", (sizeof(float)*N*DIM)/(1024*1024*1024.0));
-  printf("\nAllocating the following amount of memory for the distance matrix: %f GiB", (sizeof(float)*N*N)/(1024*1024*1024.0));
+    char inputFname[500];
+    unsigned int N=0;
+    unsigned int DIM=0;
+    float epsilon=0;
 
 
-  float * dataset=(float*)malloc(sizeof(float*)*N*DIM);
-  importDataset(inputFname, N, DIM, dataset);
+    if (argc != 5)
+    {
+        fprintf(stderr,"Please provide the following on the command line: N (number of lines in the file), dimensionality (number of coordinates per point), epsilon, dataset filename.\n");
+        exit(0);
+    }
+
+    sscanf(argv[1],"%d",&N);
+    sscanf(argv[2],"%d",&DIM);
+    sscanf(argv[3],"%f",&epsilon);
+    strcpy(inputFname,argv[4]);
+
+    checkParams(N, DIM);
+
+    printf("\nAllocating the following amount of memory for the dataset: %f GiB", (sizeof(float)*N*DIM)/(1024*1024*1024.0));
+    printf("\nAllocating the following amount of memory for the distance matrix: %f GiB", (sizeof(float)*N*N)/(1024*1024*1024.0));
 
 
-  //CPU-only mode
-  //It only computes the distance matrix but does not query the distance matrix
-  if(MODE==0){
-
-    // Calculate with CPU implementation
-    // TODO: IMPLEMENT CPU IMPLEMENTATION
-
-    return(0);
-  }
-
-  double tstart=omp_get_wtime();
-
-  //Allocate memory for the dataset
-  float * dev_dataset;
-  gpuErrchk(cudaMalloc((float**)&dev_dataset, sizeof(float)*N*DIM));
-  gpuErrchk(cudaMemcpy(dev_dataset, dataset, sizeof(float)*N*DIM, cudaMemcpyHostToDevice));
-
-  //For part 1 that computes the distance matrix
-  float * dev_distanceMatrix;
-  gpuErrchk(cudaMalloc((float**)&dev_distanceMatrix, sizeof(float)*N*N));
+    float * dataset=(float*)malloc(sizeof(float*)*N*DIM);
+    importDataset(inputFname, N, DIM, dataset);
 
 
-  //For part 2 for querying the distance matrix
-  unsigned int * resultSet = (unsigned int *)calloc(N, sizeof(unsigned int));
-  unsigned int * dev_resultSet;
-  gpuErrchk(cudaMalloc((unsigned int**)&dev_resultSet, sizeof(unsigned int)*N));
-  gpuErrchk(cudaMemcpy(dev_resultSet, resultSet, sizeof(unsigned int)*N, cudaMemcpyHostToDevice));
+    //CPU-only mode
+    //It only computes the distance matrix but does not query the distance matrix
+    if(MODE==0)
+    {
+        // Calculate with CPU implementation
+        // TODO: IMPLEMENT CPU IMPLEMENTATION
+
+        return(0);
+    }
+
+    double tstart=omp_get_wtime();
+
+    //Allocate memory for the dataset
+    float * dev_dataset;
+    gpuErrchk(cudaMalloc((float**)&dev_dataset, sizeof(float)*N*DIM));
+    gpuErrchk(cudaMemcpy(dev_dataset, dataset, sizeof(float)*N*DIM, cudaMemcpyHostToDevice));
+
+    //For part 1 that computes the distance matrix
+    float * dev_distanceMatrix;
+    gpuErrchk(cudaMalloc((float**)&dev_distanceMatrix, sizeof(float)*N*N));
 
 
-  //Baseline kernels
-  if(MODE==1){
-  unsigned int BLOCKDIM = BLOCKSIZE;
-  unsigned int NBLOCKS = ceil(N*1.0/BLOCKDIM);
-
-  // Call baseline kernel here
-  // TODO: IMPLEMENT GPU IMPLEMENTATION
-  }
+    //For part 2 for querying the distance matrix
+    unsigned int * resultSet = (unsigned int *)calloc(N, sizeof(unsigned int));
+    unsigned int * dev_resultSet;
+    gpuErrchk(cudaMalloc((unsigned int**)&dev_resultSet, sizeof(unsigned int)*N));
+    gpuErrchk(cudaMemcpy(dev_resultSet, resultSet, sizeof(unsigned int)*N, cudaMemcpyHostToDevice));
 
 
-  //Copy result set from the GPU
-  gpuErrchk(cudaMemcpy(resultSet, dev_resultSet, sizeof(unsigned int)*N, cudaMemcpyDeviceToHost));
+    //Baseline kernels
+    if(MODE==1)
+    {
+        unsigned int BLOCKDIM = BLOCKSIZE;
+        unsigned int NBLOCKS = ceil(N*1.0/BLOCKDIM);
 
-  //Compute the sum of the result set array
-  unsigned int totalWithinEpsilon=0;
-
-  //Write code here
-  for(int resultIndex = 0; resultIndex < N; resultIndex++)
-  {
-    totalWithinEpsilon += resultSet[resultIndex];
-  }
-
-  printf("\nTotal number of points within epsilon: %u", totalWithinEpsilon);
-
-  double tend=omp_get_wtime();
-
-  printf("\n[MODE: %d, N: %d] Total time: %f", MODE, N, tend-tstart);
+        // Call baseline kernel here
+        // TODO: IMPLEMENT GPU IMPLEMENTATION
+    }
 
 
-  printf("\n\n");
-  return 0;
+    //Copy result set from the GPU
+    gpuErrchk(cudaMemcpy(resultSet, dev_resultSet, sizeof(unsigned int)*N, cudaMemcpyDeviceToHost));
+
+    //Compute the sum of the result set array
+    unsigned int totalWithinEpsilon=0;
+
+    //Write code here
+    for(int resultIndex = 0; resultIndex < N; resultIndex += 1)
+    {
+        totalWithinEpsilon += resultSet[resultIndex];
+    }
+
+    printf("\nTotal number of points within epsilon: %u", totalWithinEpsilon);
+
+    double tend=omp_get_wtime();
+
+    printf("\n[MODE: %d, N: %d] Total time: %f", MODE, N, tend-tstart);
+
+
+    printf("\n\n");
+    return 0;
 }
 
 
 
-void warmUpGPU(){
-  printf("\nWarming up GPU for time trialing...\n");
-  cudaDeviceSynchronize();
-  return;
+void warmUpGPU()
+{
+    printf("\nWarming up GPU for time trialing...\n");
+    cudaDeviceSynchronize();
+    return;
 }
 
 void checkParams(unsigned int N, unsigned int DIM)
 {
-  if(N<=0 || DIM<=0){
-    fprintf(stderr, "\n Invalid parameters: Error, N: %u, DIM: %u", N, DIM);
-    fprintf(stderr, "\nReturning");
-    exit(0);
-  }
+    if(N<=0 || DIM<=0)
+    {
+        fprintf(stderr, "\n Invalid parameters: Error, N: %u, DIM: %u", N, DIM);
+        fprintf(stderr, "\nReturning");
+        exit(0);
+    }
 }
